51efe9f58de11f6be67b61b177855d6cd587ef73
Nisha Kumari <118532509+Nishak-061@users.noreply.github.com>
2024-09-11 00:47:30 +0530

    PollTextImage.jsx

--- FRONTEND/quiz-builder-app/src/POLL FOLDER/PollTextImage.jsx
+++ FRONTEND/quiz-builder-app/src/POLL FOLDER/PollTextImage.jsx
@@ -100,7 +100,7 @@ const PollTextImage = ({
 
     try {
       const response = await axios.post(
-        "https://quiz-app-mern-0bj4.onrender.com/api/quizzes/quiz/create",
+        `${REACT_APP_API_BASE_URL}/api/quizzes/quiz/create`,
         quizData,
         {
           headers: {
